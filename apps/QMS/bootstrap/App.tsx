c00fb0faf8ab12dac4de1ce84837c39d958f87e6
caesar <caesar.luo@silksoftware.com>
2023-05-15 16:52:34 +0800

    feat: emotion qiankun adjust

--- apps/QMS/bootstrap/App.tsx
+++ apps/QMS/bootstrap/App.tsx
@@ -1,20 +1,20 @@
 import { StyleProvider, legacyLogicalPropertiesTransformer } from '@ant-design/cssinjs'
 import { CacheProvider, ThemeProvider } from '@emotion/react'
 import { OConfigProvider, IGlobalStyled, OButton } from '@ocloud/ui'
-import createCache from '@emotion/cache'
+// import createCache from '@emotion/cache'
 
 import AppShell from '@/components/AppShell'
 
-const qiankunShadowDom: any = document.querySelector('div[data-name="react-app"]')?.shadowRoot
-const targetContainer: any = qiankunShadowDom.querySelector('qiankun-head')
+// const qiankunShadowDom: any = document.querySelector('div[data-name="react-app"]')?.shadowRoot
+// const targetContainer: any = qiankunShadowDom.querySelector('qiankun-head')
 
-const cache = createCache({
-  key: 'css',
-  container: targetContainer,
-  prepend: false,
-  speedy: false,
-  stylisPlugins: []
-})
+// const cache = createCache({
+//   key: 'css',
+//   container: targetContainer,
+//   prepend: false,
+//   speedy: false,
+//   stylisPlugins: []
+// })
 
 const App = () => {
   const handleClick = () => {
@@ -22,21 +22,21 @@ const App = () => {
   }
 
   return (
-    <CacheProvider value={cache}>
-      <ThemeProvider theme={{}}>
-        <StyleProvider transformers={[legacyLogicalPropertiesTransformer]}>
-          <OConfigProvider>
-            <IGlobalStyled />
-            <AppShell>
-              <p>Micro Sub App</p>
-              <OButton type="primary" onClick={handleClick}>
-                Add
-              </OButton>
-            </AppShell>
-          </OConfigProvider>
-        </StyleProvider>
-      </ThemeProvider>
-    </CacheProvider>
+    // <CacheProvider value={cache}>
+    <ThemeProvider theme={{}}>
+      <StyleProvider transformers={[legacyLogicalPropertiesTransformer]}>
+        <OConfigProvider>
+          <IGlobalStyled />
+          <AppShell>
+            <p>Micro Sub App</p>
+            <OButton type="primary" onClick={handleClick}>
+              Add
+            </OButton>
+          </AppShell>
+        </OConfigProvider>
+      </StyleProvider>
+    </ThemeProvider>
+    // </CacheProvider>
   )
 }
 
