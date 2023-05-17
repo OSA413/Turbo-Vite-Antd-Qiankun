e5eacc7178f66a348b7578e77c50924407361867
caesar <454451758@qq.com>
2023-05-17 22:05:57 +0800

    feat: router load qiankun

--- apps/QMS/bootstrap/App.tsx
+++ apps/QMS/bootstrap/App.tsx
@@ -1,4 +1,4 @@
-import { StyleProvider, legacyLogicalPropertiesTransformer } from '@ant-design/cssinjs'
+import { StyleProvider, legacyLogicalPropertiesTransformer, createCache } from '@ant-design/cssinjs'
 import { CacheProvider, ThemeProvider } from '@emotion/react'
 import { OConfigProvider, OApp, IGlobalStyled } from '@ocloud/ui'
 
@@ -10,6 +10,7 @@ const App = () => {
   const devModule: boolean = import.meta.env.REACT_DEV_MODULE
   const container = createContainer(devModule)
   const cache = createAppCache(devModule, container)
+  const sslCache = createCache()
 
   const getPopupContainer = () => {
     return devModule ? container : document.body
@@ -18,9 +19,12 @@ const App = () => {
   return (
     <CacheProvider value={cache}>
       <ThemeProvider theme={{}}>
-        <StyleProvider container={container} transformers={[legacyLogicalPropertiesTransformer]}>
+        <StyleProvider
+          cache={sslCache}
+          container={container}
+          transformers={[legacyLogicalPropertiesTransformer]}>
           <IGlobalStyled />
-          <OConfigProvider getPopupContainer={getPopupContainer}>
+          <OConfigProvider prefixCls="qms" getPopupContainer={getPopupContainer}>
             <OApp>
               <AppShell>
                 <p>Qiankun Sub React App</p>
